92882f5dc0f2f71022f500b724301e396384b8f1
Nour <nour_aldeen_44217@svuonline.org>
2018-04-14 15:41:06 +0300

    api and questions translation

---
+++ api/messages/ar-SY/app.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nour
+ * Date: 10/03/2018
+ * Time: 10:25 م
+ */
+
+return[
+    'Home' => 'الرئيسية',
+    'Mirath' => 'ميراث',
+    'Start' => 'ابدأ',
+    'Yes' => 'نعم',
+    'No' => 'لا',
+    'Continue' => 'متابعة',
+    'Calculate' => 'احتساب',
+    'Inheritors' => 'الورثة',
+    'Percentage' => 'النسبة المئوية',
+    'Inheritance dispensation' => 'توزيع التركة',
+    'The most accurate inheritance calculator based on the Islamic law' => 'الخدمة الأكثر دقة لحساب التركة حسب الشريعة الإسلامية',
+    'Husband' => 'الزوج',
+    'Wives' => 'الزوجات',
+    'Sons' => 'الأبناء الذكور',
+    'Daughters' => 'البنات الإناث',
+    'Grandsons' => 'الأحفاد الذكور',
+    'Granddaughters' => 'الحفيدات الإناث',
+    'Father' => 'الأب',
+    'Mother' => 'الأم',
+    'Grandfather' => 'الجد',
+    'PaternalGrandmother' => 'الجدة لأب',
+    'MaternalGrandmother' => 'الجدة لأم',
+    'FullBrothers' => 'الأخوة الأشقاء',
+    'FullSisters' => 'الأخوات الشقيقات',
+    'PaternalBrothers' => 'الأخوة لأب',
+    'PaternalSisters' => 'الأخوات لأب',
+    'MaternalBrothers' => 'الأخوة لأم',
+    'MaternalSisters' => 'الأخوات لأم',
+    'FullNephews' => 'أبناء الأخوة الأشقاء',
+    'PaternalNephews' => 'أبناء الأخوة لأب',
+    'FullUncles' => 'العموم الأشقاء للأب',
+    'PaternalUncles' => 'العموم أخوة الأب لأب',
+    'FullCousins' => 'أبناء العموم الأشقاء للأب',
+    'PaternalCousins' => 'أبناء العموم الغير أشقاء للأب',
+];
+?>
